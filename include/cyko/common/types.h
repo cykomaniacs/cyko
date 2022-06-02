eea135e29ea6098db1ca51d1fe54762b1101bbd8
eggheadedmonkey <eggheadedmonkey@gmail.com>
2022-06-02 23:06:30 +0200

    configuration update

--- include/cyko/common/types.h
+++ include/cyko/common/types.h
@@ -8,28 +8,33 @@ namespace cyko
 
 	using  bool_t = bool;
 	using  char_t = char;
-  #ifdef CYKO_BUILD_TOOLCHAIN_MSVC
+  #ifdef CYKO_BUILD_COMPILER_MSVC
   #ifdef CYKO_BUILD_X64
   using   int_t = long long int;          // 8 byte 64 bit
-  #elif  CYKO_BUILD_X32
+  #elif  CYKO_BUILD_X86
   using   int_t = long int;               // 4 byte 32 bit
   #endif
-  #elif  CYKO_BUILD_TOOLCHAIN_GCC
+  #elif  CYKO_BUILD_COMPILER_GCC
   #ifdef CYKO_BUILD_X64
   using   int_t = long long int;          // 8 byte 64 bit
-  #elif  CYKO_BUILD_X32
+  #elif  CYKO_BUILD_X86
   using   int_t = long int;               // 4 byte 32 bit
   #endif
-  #elif  CYKO_BUILD_TOOLCHAIN_CLANG
+  #elif  CYKO_BUILD_COMPILER_CLANG
   #ifdef CYKO_BUILD_X64
   using   int_t = long long int;          // 8 byte 64 bit
-  #elif  CYKO_BUILD_X32
+  #elif  CYKO_BUILD_X86
   using   int_t = long int;               // 4 byte 32 bit
   #endif
   #endif
 	using  size_t = std::size_t;
 	using  void_t = void;
 	using  null_t = std::nullptr_t;
+
+  /**
+   * @todo make cross-platform.
+   * @todo make platform / architecture dependent.
+   */
 
   using  byte_t = unsigned char;          // 1 byte  8 bit
   using  word_t = unsigned short int;     // 2 byte 16 bit
