b6626e10e9fadb87f8691684c1c96dccd6df4aec
eggheadedmonkey <eggheadedmonkey@gmail.com>
2022-06-02 19:44:57 +0200

    nuke script: generalized

--- nuke.ps1
+++ nuke.ps1
@@ -1,85 +1,68 @@
-$script:base = './bin'
-$script:keep = ( '.gitkeep' )
+#.SYNOPSIS
+# Clean build files.
+# cykomaniacs development 2022.
+#
+#.DESCRIPTION
+# Generic nuker.
+#
+#.PARAMETER keep
+# List of file-names to keep.
+#.PARAMETER base
+# Base path to the build output (ex: "./bin")
+#.PARAMETER name
+# Target names (ex: "debug", "release")
+#.PARAMETER arch
+# Target architectures (ex: "x64", "x86")
+#
+#.EXAMPLE
+# ./nuke.ps1 -base "./bin" -name "debug" -arch "x86"
+#.EXAMPLE
+# ./nuke.ps1 -base "./bin" -name "debug, "release" -arch "x86", "x64"
+#
+#.LINK
+# cyko@eggheadedmonkey.com
+#
+#.NOTES
+# Powershell ftw!
 
-#$script:conf = ( 'debug' )
-#$script:work = './bin/' + $script:type + '/x64'
-#$script:arch = ( 'x64' )
+param (
+  [Parameter(Mandatory=$false, ParameterSetName="main")]
+  [ValidateNotNullOrEmpty()]
+  [array]
+  $keep = ( ".gitkeep" ),
 
-function script:nuke {
-  param (
-    [Parameter(Mandatory=$false, DontShow)]
-    [array]
-    $keep = $script:keep, #( '.gitkeep' ),
+  [Parameter(Mandatory, ParameterSetName="main")]
+  [ValidateNotNullOrEmpty()]
+  [string]
+  $base,
 
-    [Parameter(Mandatory)]
-    [string]
-    $base,
+  [Parameter(Mandatory, ParameterSetName="main")]
+  [ValidateNotNullOrEmpty()]
+  [string[]]
+  $name,
 
-    [Parameter(Mandatory)]
-    [string]
-    $target,
-
-    [Parameter(Mandatory)]
-    [string]
-    $arch
-  )
-
-  function local:has($list, $item) {
-    $list.forEach({ if ("$_" -eq "$item") { return $true } })
-    return $false
-  }
-
-  # ./bin/debug/x64
-  $local:work = $base + '/' + $target + '/' + $arch
-
-  $(Get-ChildItem "$local:work" -Name).forEach({
-    if (local:has $keep "$_")
-    {
-      Write-Host -ForegroundColor Green "* " -NoNewline
-      Write-Host -ForegroundColor Gray "$local:work/$_"
-      Write-Host -ForegroundColor White "" -NoNewline # reset color
-    } else {
-      Write-Host -ForegroundColor Red "- " -NoNewline
-      Write-Host -ForegroundColor Gray "$local:work/$_"
-      Write-Host -ForegroundColor White "" -NoNewline # reset color
-      Remove-Item -Force -Recurse -ErrorAction SilentlyContinue "$local:work/$_"
-    }
-  })
-}
-
-
-#script:nuke -base "$script:base" -target 'debug' -arch 'x64'
-
-
-$script:x = @{ # arch
-  debug = (
-    #'x86',
-    'x64'
-  );
-
-  release = (
-    #'x86',
-    'x64'
-  );
-}
-
-$script:targets = (
-  'debug'
-  #'release'
+  [Parameter(Mandatory, ParameterSetName="main")]
+  [ValidateNotNullOrEmpty()]
+  [string[]]
+  $arch
 )
+
+# ---------------------------------------------------------------------------
+# helpers
+# ---------------------------------------------------------------------------
 
 function script:head {
   param (
     [Parameter(Mandatory)]
-    [string]
-    $target,
+    [string] # target name
+    $name,
 
     [Parameter(Mandatory)]
-    [string]
+    [string] # target architecture
     $arch
   )
   Write-Host -ForegroundColor DarkGray "> " -NoNewline
-  Write-Host -ForegroundColor Blue "$target" -NoNewline
+  Write-Host -ForegroundColor Blue "$name" -NoNewline
   Write-Host -ForegroundColor DarkCyan ":" -NoNewline
   Write-Host -ForegroundColor Blue "$arch" -NoNewline
   Write-Host -ForegroundColor DarkBlue "(" -NoNewline
@@ -88,11 +71,80 @@ function script:head {
   Write-Host "" # reset color
 }
 
-$script:targets.forEach({ $t = $_
-  $script:x[$t].forEach({
-    script:head -target $t -arch $_
-    script:nuke -base "$script:base" -target $t -arch $_
+function script:file {
+  param (
+    [Parameter(Mandatory)]
+    [string] # base path
+    $path,
+
+    [Parameter(Mandatory)]
+    [switch] # skip/keep
+    $skip,
+
+    [Parameter(Mandatory)]
+    [string] # file-name
+    $name
+  )
+
+  if ($skip)
+  {
+    Write-Host -ForegroundColor Green "* " -NoNewline
+    Write-Host -ForegroundColor Gray "$path/$name"
+  } else {
+    Write-Host -ForegroundColor Red "- " -NoNewline
+    Write-Host -ForegroundColor Gray "$path/$name"
+  } Write-Host "" -NoNewline # reset color
+}
+
+# ---------------------------------------------------------------------------
+# main function
+# ---------------------------------------------------------------------------
+
+function script:nuke {
+  param (
+    [Parameter(Mandatory=$false)]
+    [array] # skipped file-names
+    $skip = $keep,
+
+    [Parameter(Mandatory)]
+    [string] # base path
+    $path,
+
+    [Parameter(Mandatory)]
+    [string] # target name
+    $name,
+
+    [Parameter(Mandatory)]
+    [string] # target architecture
+    $arch
+  )
+
+  function has($list, $item) {
+    $list.forEach({ if ("$_" -eq "$item") { return $true } })
+    return $false
+  }
+
+  # ex: ./bin/debug/x64
+  $work = $path + '/' + $name + '/' + $arch
+
+  $(Get-ChildItem "$work" -Name).forEach({
+    if (has $skip "$_")
+    {
+      script:file -path "$work" -skip:$true  -name "$_"
+    } else {
+      script:file -path "$work" -skip:$false -name "$_"
+      Remove-Item -Force -Recurse -ErrorAction SilentlyContinue "$work/$_"
+    }
+  })
+}
+
+# ---------------------------------------------------------------------------
+# main / entry
+# ---------------------------------------------------------------------------
+
+$name.forEach({ $t = $_
+  $arch.ForEach({
+    script:head -name "$t" -arch "$_"
+    script:nuke -path "$base" -name "$t" -arch "$_"
   })
 })
-
-#New-Item "$script:work/.gitkeep"
